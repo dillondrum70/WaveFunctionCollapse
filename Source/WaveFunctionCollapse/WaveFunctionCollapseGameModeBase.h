64b4e0f8131d721ae4a921c2b36a0e49c3a5e2e6
dillondrum70 <dillon.drummond@mymail.champlain.edu>
2024-01-19 14:43:33 -0500

    Adding initial project

---
+++ Source/WaveFunctionCollapse/WaveFunctionCollapseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WaveFunctionCollapseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAVEFUNCTIONCOLLAPSE_API AWaveFunctionCollapseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
