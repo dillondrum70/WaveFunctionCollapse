e97902629b7c003bc2dba3c37dc9d38700bacd36
Dillon Drummond <dillondrum70@gmail.com>
2024-01-22 02:28:52 -0500

    Adding successful creation of adjacency lists

--- Source/WaveFunctionCollapse/WFC.cpp
+++ Source/WaveFunctionCollapse/WFC.cpp
@@ -84,31 +84,46 @@ void AWFC::BeginPlay()
 
 void AWFC::GeneratePrototypes()
 {
-	//Reserve and initialize the max amount of space needed, may not use it all
-	Prototypes.Init(FPrototype(), TileModels.Num() * 4);
+	//Reserve the max amount of space needed, may not use it all
+	Prototypes.Reserve(TileModels.Num() * 4);
 
 	InitializePrototypes();
-
-	CreateAdjacencies();
 
 	//Release extra reserved memory
 	Prototypes.Shrink();
 
+	CreateAdjacencies();
+
+	
+
 
 	//////// TEST CODE /////////
-	/*for (FPrototype p : Prototypes)
+	//Print adjacency lists
+	for (FPrototype p : Prototypes)
 	{
-		UE_LOG(LogTemp, Display, TEXT("%s"), p.
-		UE_LOG(LogTemp, Display, p.AdjacencyLists.Sides)
-	}*/
+		UE_LOG(LogTemp, Display, TEXT("%s Adjacency Lists"), *p.Name);
+		for (int i = 0; i < p.AdjacencyLists.Sides.Num(); i++)
+		{
+			UE_LOG(LogTemp, Display, TEXT("%s Side"), *UEnum::GetValueAsString((EDirection)i));
+			for (int index : p.AdjacencyLists.Sides[i].AdjacencyOptions)
+			{
+				UE_LOG(LogTemp, Display, TEXT("%s"), *Prototypes[index].Name);
+			}
+		}
+		
+	}
 }
 
 
 void AWFC::InitializePrototypes()
 {
-	for (TPair<TEnumAsByte<ETileType>, TSubclassOf<ATile>> tile : TileModels)
+	int count = 0;
+
+	for (const TPair<TEnumAsByte<ETileType>, TSubclassOf<ATile>>& tile : TileModels)
 	{
 		ATile* DefaultTile = tile.Value.GetDefaultObject();
+
+		if (!DefaultTile) { UE_LOG(LogTemp, Error, TEXT("DefaultTile is Invalid.  WFC::InitializePrototypes()")); }
 
 		bool opposite1Symmetric = (DefaultTile->TileProfiles[0].Name == DefaultTile->TileProfiles[2].Name);	// Check opposite profiles match
 		bool opposite2Symmetric = (DefaultTile->TileProfiles[1].Name == DefaultTile->TileProfiles[3].Name); // Check other opposite profiles match
@@ -179,33 +194,25 @@ void AWFC::CreateAdjacencies()
 	UE_LOG(LogTemp, Display, TEXT("Start %i  Modifier %i  End %i"), 
 		EDirection::DIR_SOUTH, ERotation::ONE_EIGHTY, GetDirRotated(EDirection::DIR_SOUTH, ERotation::ONE_EIGHTY));*/
 
-	UE_LOG(LogTemp, Display, TEXT("%i"), Prototypes.Num());
+	UE_LOG(LogTemp, Display, TEXT("Start CreateAdjacencyList"));
 
 	//Iterate over each prototype for each prototype, shrinking the used amount of TempPrototypes each time
 	for (currentIndex = 0; currentIndex < Prototypes.Num(); currentIndex++)
 	{
-		UE_LOG(LogTemp, Display, TEXT("CurrentIndex %i"), currentIndex);
-
 		ATile* ThisTile = Prototypes[currentIndex].Tile.GetDefaultObject();
 
 		//Iterate over remaining prototypes after currentIndex
 		for (testIndex = currentIndex + 1; testIndex < Prototypes.Num(); testIndex++)
 		{
-
-			UE_LOG(LogTemp, Display, TEXT("Test Index %i"), testIndex);
-
 			//Tile of testing prototype
 			ATile* OtherTile = Prototypes[testIndex].Tile.GetDefaultObject();
 
 			//Rotation of the testing prototype
 			ERotation rot = Prototypes[testIndex].Rotation;
 
-			if(!ThisTile) { UE_LOG(LogTemp, Error, TEXT("ThisTile is Invalid")); }
-			if(!OtherTile) { UE_LOG(LogTemp, Error, TEXT("OtherTiles is Invalid")); }
-
-			UE_LOG(LogTemp, Display, TEXT("TileProfiles %i"), ThisTile->TileProfiles.Num());
+			if (!ThisTile) { UE_LOG(LogTemp, Error, TEXT("ThisTile is Invalid")); return; }
+			if (!OtherTile) { UE_LOG(LogTemp, Error, TEXT("OtherTiles is Invalid")); return; }
 			
-
 			//Test adjacent tiles
 			if (ThisTile->TileProfiles[DIR_NORTH] == OtherTile->TileProfiles[GetDirRotated(EDirection::DIR_SOUTH, rot)])
 			{
@@ -232,6 +239,8 @@ void AWFC::CreateAdjacencies()
 			}
 		}
 	}
+
+	UE_LOG(LogTemp, Warning, TEXT("CreateAdjacencyList Succeeded"));
 }
 
 
